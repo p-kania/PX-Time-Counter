12402451ef2b237cb4cec468334ba0b36810384a
Przemek <przemyslawkania@macbook-pro-przemyslaw.local>
2018-12-18 16:35:22 +0100

    TimeCounter list complete with no data persistence.

---
+++ PX Time Counter/TmCuFirstViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  PX Time Counter
+//
+//  Created by Przemyslaw Kania on 18/12/2018.
+//  Copyright © 2018 Pixo. All rights reserved.
+//
+
+import UIKit
+
+class TmCuFirstViewController: UITableViewController {
+
+    
+    let itemArray = ["One", "Two", "Three"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TmCuFirstCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
